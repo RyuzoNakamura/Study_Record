b640489fdb08d9365acbe998b65109784a4c4685
RyuzoNakamura <nakamura.ryuzo.infratop@gmail.com>
2023-03-13 02:04:45 +0000

    [Ruby] 第7章 メソッド について学習

---
+++ ruby/method.rb
@@ -0,0 +1,11 @@
+# puts "WEBCAMP".length
+# puts "WEBCAMP".reverse
+# puts "WEBCAMPでプログラミング学習".include?("WEBCAMP")
+# puts "WEBCMPでプログラミング学習".include?("WEBCAMP")
+# puts
+# puts "WEBCAMPでプログラミング学習".methods
+# Pane is deadという表示は、cloud9が何らかの理由でパフォーマンス低下が起こったり、クラッシュしたときに表示される
+
+# 演習
+str = "webcamp"
+puts str.upcase
